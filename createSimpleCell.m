914d5e569b53a1dee05711c7483bb82b58ce7125
Bryan <bryandelgado99@yahoo.com>
2018-07-26 03:08:46 -0700

    July 26, 2018

---
+++ createSimpleCell.m
@@ -0,0 +1,23 @@
+function cell = createSimpleCell(N, R_cell, R_nuc)
+
+
+
+end
+
+%{
+function cell = createSimpleCell(points, cellRadius, nucRadius)
+
+Parameters:
+
+N - number of initial points
+
+R_cell - cell radius
+
+R_nuc - nucleus radius
+
+Returns:
+
+cell - a multidimensional array containing all points of interest on the
+cell
+
+%}
