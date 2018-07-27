097a9e4015c959ac4a81be19656600533a43c3a5
Bryan <bryandelgado99@yahoo.com>
2018-07-27 16:58:39 -0700

    July 27, 2018

---
+++ MemPoint.m
@@ -0,0 +1,18 @@
+classdef MemPoint
+   properties   
+      Position; %x,y position
+      MN_One; %MN = Membrane Neighbor
+      MN_Two;
+      NN; %NN = Nucleus Neighbor
+   end
+   methods
+      function x = Placehold1(obj)
+         %do something
+         x=0;
+      end
+      function y = Placehold2(obj,n)
+         %do something
+         y=0;
+      end
+   end
+end
---
+++ NucPoint.m
@@ -0,0 +1,18 @@
+classdef NucPoint
+   properties   
+      Position; %x,y position
+      NN_One; %NN = Nucleus Neighbor
+      NN_Two;
+      MN; %MN = Membrane Neighbor
+   end
+   methods
+      function x = Placehold1(obj)
+         %do something
+         x=0;
+      end
+      function y = Placehold2(obj,n)
+         %do something
+         y=0;
+      end
+   end
+end
