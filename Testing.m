d0e43eadde9427a52419d7c78db8e30b38265c46
Bryan <bryandelgado99@yahoo.com>
2018-08-30 16:08:37 -0700

    August 30, 2018

--- Testing.m
+++ Testing.m
@@ -1,4 +1,24 @@
-cell = createSimpleCell(5, 5, 12, 4, .5);
-%test = cell.NucleusPoints{1,1};
-cell.grow();
-render(cell);
+C = Cell(15, 5, 5,  1.0e-6, 5.0e-6);
+%render(C);
+
+%render(C);
+%pause(1);
+%C.recalc_springs("dec");
+%C.recalc_springs("dec");    
+%render(C);
+C.calc_vol();
+for i=1:500
+    fprintf('\n Iteration: %f \n' , i);
+    C.check_vol();
+    C.calc_cell_force();
+    C.apply_cell_force(.25);
+    C.calc_vol();
+    if mod(i,250) == 0
+      % render(C); 
+    end
+end
+render(C);
+
+
+
+
